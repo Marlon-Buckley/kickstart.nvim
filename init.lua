7d6286b8104bcc5f0d9d0ebf43b28662d201372e
marlon-buckley <marlon.buckley@protonmail.com>
2023-11-08 10:28:00 +0000

    centre screen after scroll

--- init.lua
+++ init.lua
@@ -285,6 +285,8 @@ vim.o.termguicolors = true
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+vim.keymap.set("n", "<C-d>", "<C-d>zz", {desc = "Center cursor after moving down half-page"})
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
